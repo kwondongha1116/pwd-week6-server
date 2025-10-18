5a2734b5c9725521a3d733fdcd1929d61e7d1fd3
kwondongha1116 <dongha.kwon1116@gmail.com>
2025-10-18 21:20:31 +0900

    week6

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: ['node_modules', 'src/data'],
+  moduleFileExtensions: ['js', 'json'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+//
